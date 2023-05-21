b6eda14b8ee90899241138d1aeaf60a4a1819651
iqra-ali <iqra.ali@student.moringaschool.com>
2023-05-21 12:59:11 +0300

    to be reviewed

--- lib/book.py
+++ lib/book.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# $ pytest -x testing/book_test.py
 class Book:
     pass
     
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-
+# $ pytest -x testing/shoe_test.py
 class Shoe:
     pass
