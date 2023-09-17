b9a100c06cebf0ee52256ab43dedb6770d6cd02d
Iqra <iqra.ali@student.moringaschool.com>
2023-09-17 17:34:00 +0300

    bookpy

--- lib/book.py
+++ lib/book.py
@@ -3,10 +3,16 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
+
 # private variable to store page_count
         self._page_count = None  
 # Seting the page_count using the setter method
         self.page_count = page_count
+# A list to store reviews
+        self.reviews = []
+# Calculate average rating
+        self.average_rating = None
+        
     @property
     def page_count(self):
         return self._page_count
@@ -19,5 +25,12 @@ class Book:
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
     pass
+
+    def add_review(self, review_message, rating):
+        #adding a review and rating the my book
+        self.reviews.append({"text": review_message, "rating": rating})
+        #calculating the average rating
+        if self.reviews:
+            self.average_rating = sum(review["rating"] for review in self.reviews) / len(self.reviews) 
     
         
